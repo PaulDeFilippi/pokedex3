95e57fa7fa613db61f636a53d04dbd332966bb8b
Paul Defilippi <BlindFaith@pauls-imac.home>
2016-09-29 17:59:03 -0400

    added connectivity to a web REST API, data parsing, dynamically updating labels and IBOutlets

---
+++ pokedex3/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex3
+//
+//  Created by Paul Defilippi on 9/28/16.
+//  Copyright © 2016 Paul Defilippi. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "https://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
--- pokedex3/PokemonDetailVC.swift
+++ pokedex3/PokemonDetailVC.swift
@@ -12,36 +12,48 @@ class PokemonDetailVC: UIViewController {
     
     var pokemon: Pokemon!
     
-    
     @IBOutlet weak var nameLbl: UILabel!
-    
     @IBOutlet weak var mainImg: UIImageView!
-
     @IBOutlet weak var descriptionLbl: UILabel!
-    
     @IBOutlet weak var typeLbl: UILabel!
-    
     @IBOutlet weak var defenseLbl: UILabel!
-    
     @IBOutlet weak var heightLbl: UILabel!
-    
     @IBOutlet weak var pokedexLbl: UILabel!
-    
     @IBOutlet weak var weightLbl: UILabel!
-    
     @IBOutlet weak var attackLbl: UILabel!
-    
     @IBOutlet weak var currentEvoImg: UIImageView!
-    
     @IBOutlet weak var nextEvoImg: UIImageView!
-    
     @IBOutlet weak var evoLbl: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         nameLbl.text = pokemon.name
-
+        
+        pokemon.downloadPokemonDetail {
+            
+            // Only called after the network call is complete
+            
+            
+            // print used below to show that we did arrive inside the function
+            print("Did arrive here?")
+            self.updateUI()
+            
+            // now that we are arriving inside the downloadPokemonDetail function we can start updating the UI.  We are getting the data in the right place and can start updating labels
+            
+        }
+    
+    }
+    
+    func updateUI() {
+        
+        attackLbl.text = pokemon.attack
+        defenseLbl.text = pokemon.defense
+        heightLbl.text = pokemon.height
+        weightLbl.text = pokemon.weight
+        
+        
+        
         
     }
     
